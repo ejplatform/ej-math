d623f45ab5fcf3abb1eaac5b023b91d4a2e943ac
Luan Guimarães <luang@protonmail.ch>
2017-12-11 16:01:49 -0200

    Add some method's documentation

--- src/pushtogether_math/decomposer.py
+++ src/pushtogether_math/decomposer.py
@@ -1,6 +1,9 @@
 from sklearn import decomposition
 
 def pca_decompose(dataset, n=2):
+    """
+    Applies the PCA to reduce the dimensionality of the dataset
+    """
     pca = decomposition.PCA(n_components=n)
     pca.fit(dataset)
     transformed_dataset = pca.transform(dataset)
